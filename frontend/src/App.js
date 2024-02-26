ccfbe48d544f3f6aee99f1d59f7796a4658451ff
Timothy Kim <timothy.e.kim@sjsu.edu>
2024-02-25 22:56:45 -0800

    Adding navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
+import homePage from '/Users/timkim/Desktop/CMPE133/Recipe-Radar/frontend/src/homepage/homepage.js';
 import './App.css';
-
+import navbar from './components/navigation';
+//<Route path='/' exact component={Home} />
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <navbar/>
+      <Switch>
+        <Route path = '/homepage.js' exact component = {homePage}></Route>
+      </Switch>
+    </Router>
+    </>
   );
 }
 
